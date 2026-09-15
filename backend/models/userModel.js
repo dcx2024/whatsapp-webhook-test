64367fb38ae618788f7bf8db2f1e15066a39fdf1
dcx2024 <dcx2607@gmail.com>
2026-09-15 10:12:59 +0100

    Changed user model for ge seller id from seller_id to id

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -14,7 +14,7 @@ const Seller={
     },
 
     async getSellerId(phone_no){
-        const seller=await db(TABLE).select('seller_id')
+        const seller=await db(TABLE).select('id')
             .where({ phone_no })
             .first(); 
         return seller;
